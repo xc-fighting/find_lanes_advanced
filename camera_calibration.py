a5fb5711e4d93a213ab0dc3f46cb88dd091f9081
ChenXuOffWorld <chen.xu@offworld.ai>
2021-04-25 21:41:42 -0700

    Use python file directly for pipeline of camera calibration

---
+++ camera_calibration.py
@@ -0,0 +1,33 @@
+import pickle
+import cv2
+import numpy as np
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import glob
+nx = 9 # the number of inside corners in x
+ny = 6 # the number of inside corners in y
+objpoints = [] # 3d points in world space(meter with z axis)
+imgpoints = [] # 2d points in pixel(image) space
+images = glob.glob('./camera_cal/calibration*.jpg')
+size = nx*ny
+objp = np.zeros((size,3),np.float32)
+for row in range(0,6):
+	for col in range(0,9):
+		index = row*nx + col
+		objp[index][0] = col
+		objp[index][1] = row
+print(objp)
+for frame in images:
+    img = mpimg.imread(frame)
+    print("[dbg]:the name of picture:"+frame)
+    filename = frame.split('/')[2]
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    ret,corners = cv2.findChessboardCorners(gray,(9,6),None)
+    if(ret == True):
+    	imgpoints.append(corners)
+    	objpoints.append(objp)
+    	img = cv2.drawChessboardCorners(img,(9,6),corners,ret)
+    	cv2.imwrite('./camera_chessboard/'+filename,img)
+    else:
+    	print(filename+":have error in return")
